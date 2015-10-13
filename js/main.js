f041ebae287911405ccbe260be144e4b1629c7a8
@tzaluzhnuy <@sinout96@hotmail.com>
2015-10-13 16:51:58 +0300

    footer&nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function (){
+   var nav = $('.navbar-default').offset().top;
 
+    $(document).scroll(function(){
+        if($(this).scrollTop() > nav)
+        {
+            $('.navbar-default').addClass('dark-nav');
+        } else {
+            $('.navbar-default').removeClass('dark-nav');
+        }
+    });
+});
